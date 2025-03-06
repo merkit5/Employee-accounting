b6c8f3c0e8c21d31291e8e77a04382e1b0a93ca6
papae <papaevmix@mail.ru>
2025-03-06 19:57:44 +0300

    add router employee

---
+++ router/employee.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Employee = require('../model/Employee');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const employee = await Employee.findAll();
+        res.json(employee);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const employee = await Employee.findByPk(req.params.id);
+        if (employee) {
+            res.json(employee);
+        } else {
+            res.status(404).json({ error: 'Employee not found'});
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
