66fa100c0ff75d199b2bd7ec357ae284d0b55c5d
papae <papaevmix@mail.ru>
2025-03-09 20:01:42 +0300

    Add an employee edit form

--- frontend/script.js
+++ frontend/script.js
@@ -15,6 +15,9 @@ async function getEmployees() {
                 <td>${employee.post}</td>
                 <td>${employee.salary}</td>
                 <td>${new Date(employee.employment_date).toLocaleDateString()}</td>
+                <td>
+                    <button onclick="loadEmp(${employee.id})">Редактировать</button>
+                </td>
             </tr>`
         ).join('');
     } catch (error) {
@@ -42,10 +45,55 @@ async function addEmployee(event) {
 
         await getEmployees();
     } catch (error) {
+        console.error('Ошибка:', error);
+    }
+}
 
+
+async function loadEmp(id) {
+    try {
+        const response = await fetch(`/api/employee/${id}`);
+        const employee = await response.json();
+        fillForm(employee);
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+}
+
+function fillForm(employee) {
+    const editForm = document.getElementById('editEmpForm');
+    editForm.style.display = 'block';
+
+    Object.keys(employee).forEach(key => {
+        editForm.elements[key].value = key.includes('_date') ? employee[key].split('T')[0] : employee[key];
+    });
+}
+
+async function saveEmployee(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    data.salary = parseFloat(data.salary);
+
+    try {
+        const response = await fetch(`/api/employee/${data.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        const editForm = document.getElementById('editEmpForm');
+        editForm.style.display = 'none';
+        await getEmployees();
+    } catch (error) {
         console.error('Ошибка:', error);
     }
 }
 
 document.getElementById('addEmpForm').addEventListener('submit', addEmployee);
+document.getElementById('editEmpForm').addEventListener('submit', saveEmployee);
+document.getElementById('cancelEdit').addEventListener('click', () => {document.getElementById('editEmpForm').style.display = 'none';});
 document.addEventListener('DOMContentLoaded', getEmployees);
